8efdfe8516c4c44647d7157ca8584278c7e3f926
Aline Fernandes <fernandes.dev@outlook.com>
2025-05-24 19:41:13 -0300

    usado classe para calcular triangulo

---
+++ classe-calculo-triangulo/Triangulo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace classe_calculo_triangulo
+{
+    public class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
