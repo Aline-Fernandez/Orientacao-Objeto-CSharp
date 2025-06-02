6e2103bd6a03b64fa8c99317279006c892ac4328
Aline Fernandes <fernandes.dev@outlook.com>
2025-06-02 10:55:28 -0300

    Calculo de area, parínetro e Diagonal.

---
+++ valor-area/Program.cs
@@ -0,0 +1,19 @@
+﻿using valor_area;
+using System.Globalization;
+
+Retangulo retangulo = new Retangulo();
+
+Console.WriteLine("Entre com a largura do retângulo: ");
+retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Entre com a altura do retângulo: ");
+retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double area = retangulo.Area();
+double perimetro = retangulo.Perimetro();
+double diagonal = retangulo.Diagonal();
+
+Console.WriteLine("Área: " + area.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Perímetro: " + perimetro.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Diagonal: " + diagonal.ToString("F2", CultureInfo.InvariantCulture));
+
---
+++ valor-area/Retangulo.cs
@@ -0,0 +1,26 @@
+﻿
+namespace valor_area
+{
+    public class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+        public double Area()
+        {
+            double a = (Largura * Altura);
+            return a;
+        }
+        public double Perimetro()
+        {
+            double p = 2 * (Largura + Altura);
+            return p;
+        }
+        public double Diagonal()
+        {
+            double d = Math.Sqrt((Largura * Largura) + (Altura * Altura));
+            return d;
+        }
+    }
+}
+
+
