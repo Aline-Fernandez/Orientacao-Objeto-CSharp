80f92dcdc341f77694e9a5168ddf284b7f195323
Aline Fernandes <fernandes.dev@outlook.com>
2025-06-11 17:02:19 -0300

    Cálculo de salário líquido e atualização com aumento percentual

---
+++ aumento-salarial-imposto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+namespace aumento_salarial_imposto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);                      
+
+            Console.WriteLine();
+            Console.Write("Funcionário: " + funcionario);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentoSalario(porcent);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + funcionario);            
+
+        }
+    }
+}
