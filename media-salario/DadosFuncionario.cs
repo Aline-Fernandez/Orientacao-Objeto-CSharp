75bffd8f350baf807b9ff3c2a505f9b87ca05a65
Aline Fernandes <fernandes.dev@outlook.com>
2025-06-11 14:23:44 -0300

    Calculo da média do salário de dois funcionários.

---
+++ media-salario/DadosFuncionario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace media_salario
+{
+    internal class DadosFuncionario
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
---
+++ media-salario/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+namespace media_salario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DadosFuncionario funcionario1 = new DadosFuncionario();
+            DadosFuncionario funcionario2 = new DadosFuncionario();
+
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            funcionario1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do segundo funcionario: ");
+            Console.Write("Nome: ");
+            funcionario2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.WriteLine();
+            double media = (funcionario1.Salario + funcionario2.Salario) / 2.0;
+
+            Console.WriteLine("A média salarial é: " + media.ToString("F2", CultureInfo.InvariantCulture)); 
+            
+        }
+    }
+}
