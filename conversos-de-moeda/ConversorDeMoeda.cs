6e77ca4c7bdf1dfb3d0ea07c5e72cb6d84c7cd44
Aline Fernandes <fernandes.dev@outlook.com>
2025-07-01 18:13:02 -0300

    Informar quantos dólares vai comprar e mostra o valor em Reais com IOF.

---
+++ conversos-de-moeda/ConversorDeMoeda.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace conversos_de_moeda {
+    internal class ConversorDeMoeda {
+        public static double Cota = 3.10;
+
+        public static double Reais(double dolar) {
+            return Cota * dolar;
+        }
+        public static double Iof(double dolar) {
+            return Cota * dolar * 1.06;
+        }
+    }
+    
+}
---
+++ conversos-de-moeda/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using conversos_de_moeda;
+
+namespace conversor_dolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos dólares você vai comprar? ");            
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double real = ConversorDeMoeda.Reais(dolar);
+            double iof = ConversorDeMoeda.Iof(dolar);
+
+            Console.WriteLine("Valor da cóta do dólar: " + ConversorDeMoeda.Cota.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor a ser pago em reais: " + iof.ToString("F2", CultureInfo.InvariantCulture));
+
+           
+        }
+    }
+}
